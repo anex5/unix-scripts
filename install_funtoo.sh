e72eed252d60814d743a9e29fc3c1a65dd01e4cd
anex5 <anex5.2008@gmail.com>
2018-05-13 21:51:09 +0300

    Added new features. not tested.
    Signed-off-by: anex5 <anex5.2008@gmail.com>

--- install_funtoo.sh
+++ install_funtoo.sh
@@ -6,12 +6,14 @@ base_dir=$( dirname "${BASH_SOURCE[0]}" )
 saved_file="$( basename "${BASH_SOURCE[0]}" ).prev"
 tempfs="/mnt/tempfs/fsimage"
 cfg_prefix="._cfg0000_"
+local_overlay_name="local"
 
 write_save=1
 work_dir=
 root_part=
 boot_part=
 swap_part=
+tempfs_part=
 hostname=
 locales=
 timezone=
@@ -33,14 +35,17 @@ part_list="$(get_partitions_list)"
 
 if ! [ -a "${work_part}" ]; then
 	if execution_premission "Use virtual disk image? "; then
+		echo
+		read -p "Enter location of virtual disk image (default: ${tempfs})" tempfs
+
 		options="${part_list}"
-		if prompt_select "Select partition for ${tempfs}."; then
-			work_part="${selected}"
-			test ${write_save} && { save_var "work_part" ${saved_file}; }
+		if prompt_select "Select partition containing ${tempfs}."; then
+			tempfs_part="${selected}"
+			test ${write_save} && { save_var "tempfs_part" ${saved_file}; }
 		fi
 		echo "Mounting work partition ${tempfs%\/*}"
 		try mkdir -p ${tempfs%\/*} && cleanup rm -r ${tempfs%\/*}
-		try mount ${work_part} ${tempfs%\/*} && { cleanup wait_umount ${tempfs%\/*}; cleanup umount ${tempfs%\/*}; }
+		try mount ${tempfs_part} ${tempfs%\/*} && { cleanup wait_umount ${tempfs%\/*}; cleanup umount ${tempfs%\/*}; }
 		if [ ! -w ${tempfs} ]; then
 			echo
 			read -p "Enter size of new virtual disk image (M) " tempfs_size
@@ -55,14 +60,16 @@ fi
 if ! [ -d "${work_dir}" ]; then
 	fdisk -l
 	options="$(find /dev/* -maxdepth 0 -name "sd?" -or -name "hd?") skip"
-	if prompt_select "Select disk for partitioning."; then
-		work_disk="${selected}"
-		test ${write_save} && { save_var "work_disk" ${saved_file}; }
-		options="fdisk gdisk skip"
-		if prompt_select "Select partitioning programm."; then
-			try ${selected} ${work_disk}
+	while execution_premission "Partition disks? "; do
+		if prompt_select "Select disk for partitioning."; then
+			work_disk="${selected}"
+			test ${write_save} && { save_var "work_disk" ${saved_file}; }
+			options="fdisk gdisk skip"
+			if prompt_select "Select partitioning programm."; then
+				try ${selected} ${work_disk}
+			fi
 		fi
-	fi
+	done
 fi
 
 #if [ -w "${tempfs}" ]; then
@@ -232,7 +239,6 @@ if execution_premission "Install config files? "; then
 	for locale in ${locales} 
 	do
 		echo "${locale} UTF-8" >> ${work_dir}/etc/${cfg_prefix}locale.gen
-		#sed "$ a ${locale} UTF-8" ${work_dir}/etc/locale.gen > ${work_dir}/etc/${cfg_prefix}locale.gen
 	done
 
 	if [ -z "${keymap}" ]; then
@@ -253,10 +259,6 @@ if execution_premission "Install config files? "; then
 	sed -e "s|^\(MAKEOPTS=\).*|\1\"-j"$(( $(nproc)+1 ))" --quiet\"|" ${work_dir}/etc/portage/make.conf > ${work_dir}/etc/portage/${cfg_prefix}make.conf
 	sed -i "s|^\(LINGUAS=\).*|\1\""${locales//_*/}"\"|" ${work_dir}/etc/portage/${cfg_prefix}make.conf
 	sed -i "s|^\(L10N=\).*|\1\""${locales//_*/}"\"|" ${work_dir}/etc/portage/${cfg_prefix}make.conf
-
-	#mkdir -p ${work_dir}/var/db/repos/funtoo/profiles/funtoo
-	#mkdir -p ${work_dir}/var/db/repos/gentoo/profiles/gentoo
-	#mkdir -p ${work_dir}/etc/portage/repos.conf
 fi
 
 if execution_premission "Enable dhcp network? "; then
@@ -266,6 +268,27 @@ if execution_premission "Enable dhcp network? "; then
 	cp /etc/resolv.conf ${work_dir}/etc/resolv.conf
 	echo "nameserver 8.8.8.8" >> ${work_dir}/etc/resolv.conf
 	echo "nameserver 8.8.4.4" >> ${work_dir}/etc/resolv.conf
+fi
+
+if execution_premission "Create blank local overlay? "; then
+	echo
+	read -p "Enter name of the local overlay (default: "${local_overlay_name}") " local_overlay_name
+	mkdir -p ${work_dir}/var/overlay/${local_overlay_name}
+	git clone https://github.com/funtoo/skeleton-overlay.git ${work_dir}/var/overlay
+	mv ${work_dir}/var/overlay/skeleton-overlay ${work_dir}/var/overlay/${local_overlay_name}
+	echo ${local_overlay_name} > ${work_dir}/var/overlay/${local_overlay_name}/profiles/repo_name
+	echo "masters = core-kit" >> ${work_dir}/var/overlay/${local_overlay_name}/metadata/layout.conf
+	echo -e "[DEFAULT]/nmain-repo = core-kit/n["${local_overlay_name}"]\
+	/nlocation = /var/overlay/${local_overlay_name}\
+	/nauto-sync = no/npriority = 10/n" > ${work_dir}/etc/portage/repos.conf/${local_overlay_name}.conf
+fi
+
+if execution_premission "Remove /usr/share/{man,doc,gtk-doc} directories, and remove man packages from minimal system profile?"; then
+	rm -r ${work_dir}/usr/share/{man,doc,gtk-doc}
+	packages_path = ${work_dir}/var/git/meta-repo/kits/core-kit/profiles/funtoo/1.0/linux-gnu/flawor/minimal/packages
+	sed -i "s|^\(\*virtual/man\).*|\1\#\*virtual/man|" ${packages_path}
+	sed -i "s|^\(\*sys-apps/man-pages\).*|\1\#\*sys-apps/man-pages|" ${packages_path}
+	chmod +i ${packages_path}
 fi
 
 if execution_premission "Chroot in the new system environment? "; then
@@ -282,10 +305,9 @@ if execution_premission "Chroot in the new system environment? "; then
 	\nsync portage tree and merge packages you need\" \n\
 	rm -rf ~/.bash_login"
 	echo -e ${profile} > ${work_dir}/root/.bash_login
-
+	env -i HOME=/root TERM=$TERM SHELL=/bin/bash chroot ${work_dir} /root/.bash_login
 	env -i HOME=/root TERM=$TERM SHELL=/bin/bash chroot ${work_dir} /bin/bash --login
 fi
-
 
 proceed_cleanup
 exit 0
