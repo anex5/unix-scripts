40710f629f8464ea0bdc773aeb15702b056d3d16
anex5 <anex5.2008@gmail.com>
2018-05-27 14:12:59 +0300

    Some improvements and bugfixes. Minor test passed.
    Signed-off-by: anex5 <anex5.2008@gmail.com>

--- install_funtoo.sh
+++ install_funtoo.sh
@@ -26,14 +26,15 @@ execution_premission "Sure you want to run this? " || die
 
 if [ -f "${saved_file}" ]; then
 	saved_file="$(find ${base_dir} ~/* -name ${saved_file} -type f)"
-	echo ${saved_file}
+	#echo ${saved_file}
 	execution_premission "Continue previous installation?" && { source ${saved_file} || echo "" > ${saved_file}; }
 	cleanup rm -r ${saved_file}
 fi
 
 part_list="$(get_partitions_list)"
 
-if ! [ -a "${work_part}" ]; then
+#echo tempfs_part=${tempfs_part}
+if ! [ -e "${tempfs_part}" ] || ! [ ${tempfs_part} == "skip" ]; then
 	if execution_premission "Use virtual disk image? "; then
 		echo
 		read -p "Enter location of virtual disk image (default: ${tempfs})" tempfs
@@ -49,17 +50,22 @@ if ! [ -a "${work_part}" ]; then
 		if [ ! -w ${tempfs} ]; then
 			echo
 			read -p "Enter size of new virtual disk image (M) " tempfs_size
-			echo "Creating virtual disk image ${tempfs} of ${tempfs_size}M..."
+			echo "Creating raw virtual disk image ${tempfs} of ${tempfs_size}M..."
 			pv -EE -s ${tempfs_size}M -S -B 4k /dev/zero > ${tempfs} || echo "Cannot create virtual disk image ${tempfs} of ${tempfs_size}M"
 		fi
 		work_disk=$(losetup -f)
 		try losetup -P ${work_disk} ${tempfs} && cleanup losetup -d ${work_disk}
+	else
+		tempfs_part="skip"
+		test ${write_save} && { save_var "tempfs_part" ${saved_file}; }
 	fi
 fi
 
+disk_list="$(lsblk -d -r | awk 'NR>1 { print $1 }') skip"
+
 if ! [ -d "${work_dir}" ]; then
 	fdisk -l
-	options="$(find /dev/* -maxdepth 0 -name "sd?" -or -name "hd?") skip"
+	options="${disk_list}"
 	while execution_premission "Partition disks? "; do
 		if prompt_select "Select disk for partitioning."; then
 			work_disk="${selected}"
@@ -90,7 +96,7 @@ if ! [ -d "${work_dir}" ]; then
 	fi
 fi
 
-if ! [ -a "${root_part}" ]; then
+if ! [ -e "${root_part}" ]; then
 	options="${part_list}"
 	if prompt_select "Select root partition."; then
 		root_part="${selected}"
@@ -103,7 +109,7 @@ fi
 
 part_list="${part_list//${root_part}/}"
 
-if ! [ -a "${boot_part}" ]; then
+if ! [ -e "${boot_part}" ]; then
 	if [ -n "${part_list//[[:cntrl:]]/}" ]; then
 		options="${part_list} skip"
 		if prompt_select "Select boot partition."; then
@@ -114,10 +120,10 @@ if ! [ -a "${boot_part}" ]; then
 	fi
 fi
 
-disk_list="$(find /dev/* -maxdepth 0 -name "sd?" -or -name "hd?")"
+#disk_list="$(find /dev/* -maxdepth 0 -name "sd?" -or -name "hd?")"
 part_list="${disk_list//${root_part%[[:digit:]]*}/} ${part_list//${boot_part}/}"
 
-if ! [ -a "${swap_part}" ]; then
+if ! [ -e "${swap_part}" ]; then
 	if [ -n "${part_list//[[:cntrl:]]/}" ]; then
 		options="${part_list} skip"
 		if prompt_select "Select swap partition."; then
@@ -132,19 +138,26 @@ fi
 printf "\nMounting partitions. \n"
 try mount ${root_part} ${work_dir} && { cleanup wait_umount ${work_dir}; cleanup umount -d ${work_dir}; }
 try mkdir -p ${work_dir}/boot
-try mount ${boot_part} ${work_dir}/boot && { cleanup wait_umount ${work_dir}/boot; cleanup umount -d ${work_dir}/boot; }
+! [ -e ${boot_part} ] && try mount ${boot_part} ${work_dir}/boot && { cleanup wait_umount ${work_dir}/boot; cleanup umount -d ${work_dir}/boot; }
 
 
 if ! [ -f "${stage}" ]; then
 	if execution_premission "Download new stage?"; then
 		printf "\nLoading file list..."
-		stage_list=$(wget -m -r -np -nd -e robots=off --spider -l4 -A ${stage_name} ${stage_site} 2>&1 | grep -Eio http.+${stage_name})
+		stage_list=$(wget -m -r -np -nd -e robots=off --spider -l4 -A ${stage_name} ${stage_site} 2>&1 | grep -Eio https\:\/\/.+${stage_name})
 		#cleanup "rm -r ${stage_site}"
 		options="${stage_list} skip"
 		if prompt_select "Select stage to download."; then
-			try download ${selected} ${work_dir} && cleanup rm -r "${work_dir}/${selected##*\/}" || echo "Cannot download ${selected}"
+			echo ${work_dir}/${selected##*\/}
+			try download ${selected} ${work_dir} && { cleanup rm -r "${work_dir}/${selected##*\/}"; cleanup rm -r ~/.wget-hsts; cleanuo rm ~/index*.tmp; } || echo "Cannot download ${selected##*\/}"
+			#try download "${selected}.DIGESTS.asc" ${work_dir} && cleanup rm -r "${work_dir}/${selected##*\/}.DIGESTS.asc" || echo "Cannot download ${selected##*\/}.DIGESTS.asc"
+			#try download "${selected}.DIGESTS" ${work_dir} && cleanup rm -r "${work_dir}/${selected##*\/}.DIGESTS" || echo "Cannot download ${selected##*\/}.DIGESTS"
 			try download ${selected}".hash.txt" ${work_dir} && cleanup rm -r "${work_dir}/${selected##*\/}.hash.txt" || echo "Cannot download ${selected}.hash.txt"
-			try echo $(cat ${work_dir}/${selected##*\/}.hash.txt | awk '{ print $2 }') ${work_dir}/${selected##*\/} | sha256sum -c -
+
+			#gpg --keyserver hkp://keys.gnupg.net --recv-keys 0xBB572E0E2D182910
+			#gpg --verify ${work_dir}/${selected##*\/}.DIGESTS.asc
+			echo "Verifing checksum..."
+			try echo $(cat ${work_dir}/${selected##*\/}.hash.txt | awk -F'  ' '{ if ($1 ~ "SHA512") next; print $1; exit}') ${work_dir}/${selected##*\/} | sha256sum -c -
 		fi
 	fi
 fi
@@ -154,7 +167,7 @@ if [ -n "${stage_list}" ]; then
 	options="${stage_list} skip"
 	if prompt_select "Select stage to extract."; then
 		stage="${selected}"
-		decrunch ${stage} ${work_dir} || die "Cannot extract ${stage}"
+		decrunch --xattrs-include='*.*' --numeric-owner ${stage} ${work_dir} || die "Cannot extract ${stage}"
 		test ${write_save} && { save_var "stage" ${saved_file}; }
 	fi
 else
@@ -174,6 +187,9 @@ if [ -n "${template_list}" ]; then
 			output=$(sed -n "2,$ p" ${file})
 			if [ $? -eq 0 ]; then
 				if [ ! -d "${orig_file%\/*}" ]; then 
+					if [ -e "${orig_file%\/*}" ] || [ -L "${orig_file%\/*}" ]; then 
+						try rm ${orig_file%\/*} 
+					fi
 					try mkdir -p "${orig_file%\/*}"
 				fi
 				echo "${output}" > ${orig_file}
@@ -182,13 +198,25 @@ if [ -n "${template_list}" ]; then
 				echo "sed \"2,\$ p\" ${file} failed"
 			fi
 		done
+		#repos_list="$(find ${work_dir}/etc/portage/repos.conf -type f )"
+		#echo ${repos_list}
+		#for file in ${repos_list}
+		#do
+		#	overlay_location=$(cat ${file} | awk -F= '{ if ($1 ~ "location") print $2}')
+		#	echo ${overlay_location}
+		#	if ! [ -d ${overlay_location} ]; then
+		#		try mkdir -p ${overlay_location}
+		#	fi
+		#done
 	fi
 fi
 
-if execution_premission "Install config files? "; then
+if execution_premission "Edit config files? "; then
+
 	if [ -z "${password}" ]; then
 		read -p "Enter new password for root " password
-		test ${write_save} && { save_var "password" ${saved_file}; }
+		test ${write_save} && ! [ -z password ] && { save_var "password" ${saved_file}; }
+
 	fi
 	shadow="$(openssl passwd -1 ${password}):$(( $(date +%s)/86400 )):0:::::"
 	sed -e "s|^\(root:\).*|\1"${shadow}"|" ${work_dir}/etc/shadow > ${work_dir}/etc/${cfg_prefix}shadow
@@ -251,7 +279,9 @@ if execution_premission "Install config files? "; then
 	fi
 	sed -e "s|^\(keymap=\).*|\1\""${keymap}"\"|" ${work_dir}/etc/conf.d/keymaps > ${work_dir}/etc/conf.d/${cfg_prefix}keymaps
 	sed -e "s|^\(consolefont=\).*|\1\"ter-u16b\"|" ${work_dir}/etc/conf.d/consolefont > ${work_dir}/etc/conf.d/${cfg_prefix}consolefont
-	ln -rs /etc/init.d/conslefont ${work_dir}/etc/runlevels/boot/consolefont
+	if [ -a ${work_dir}/etc/runlevels/boot/consolefont ]; then
+		ln -rs /etc/init.d/conslefont ${work_dir}/etc/runlevels/boot/consolefont
+	fi
 
 	fstabgen "${root_part}:/:defaults:0:1 ${boot_part}:/boot:noauto,noatime:1:2 ${swap_part}:swap:sw:0:0" "${work_dir}/etc/fstab"
 
@@ -272,23 +302,26 @@ fi
 
 if execution_premission "Create blank local overlay? "; then
 	echo
-	read -p "Enter name of the local overlay (default: "${local_overlay_name}") " local_overlay_name
+	read -p "Enter name of the local overlay (default: "${local_overlay_name}") " entered_overlay_name
+	if [ -n "${entered_overlay_name}" ]; then 
+		local_overlay_name=${entered_overlay_name} 
+	fi
+
 	mkdir -p ${work_dir}/var/overlay/${local_overlay_name}
-	git clone https://github.com/funtoo/skeleton-overlay.git ${work_dir}/var/overlay
-	mv ${work_dir}/var/overlay/skeleton-overlay ${work_dir}/var/overlay/${local_overlay_name}
+	git clone https://github.com/funtoo/skeleton-overlay.git ${work_dir}/var/overlay/${local_overlay_name}
 	echo ${local_overlay_name} > ${work_dir}/var/overlay/${local_overlay_name}/profiles/repo_name
-	echo "masters = core-kit" >> ${work_dir}/var/overlay/${local_overlay_name}/metadata/layout.conf
-	echo -e "[DEFAULT]/nmain-repo = core-kit/n["${local_overlay_name}"]\
+	echo "masters = gentoo" >> ${work_dir}/var/overlay/${local_overlay_name}/metadata/layout.conf
+	echo -e "[DEFAULT]/nmain-repo = gentoo/n["${local_overlay_name}"]\
 	/nlocation = /var/overlay/${local_overlay_name}\
 	/nauto-sync = no/npriority = 10/n" > ${work_dir}/etc/portage/repos.conf/${local_overlay_name}.conf
 fi
 
-if execution_premission "Remove /usr/share/{man,doc,gtk-doc} directories, and remove man packages from minimal system profile?"; then
-	rm -r ${work_dir}/usr/share/{man,doc,gtk-doc}
-	packages_path = ${work_dir}/var/git/meta-repo/kits/core-kit/profiles/funtoo/1.0/linux-gnu/flavor/minimal/packages
+if execution_premission "Remove /usr/share/{info,man,doc,gtk-doc} directories, and remove man packages from minimal system profile?"; then
+	rm -r ${work_dir}/usr/share/{info,man,doc,gtk-doc}
+	packages_path=${work_dir}/var/git/meta-repo/kits/core-kit/profiles/funtoo/1.0/linux-gnu/flavor/minimal/packages
 	sed -i "s|^\(\*virtual/man\).*|\1\#\*virtual/man|" ${packages_path}
 	sed -i "s|^\(\*sys-apps/man-pages\).*|\1\#\*sys-apps/man-pages|" ${packages_path}
-	chmod +i ${packages_path}
+	chattr +i ${packages_path}
 fi
 
 if execution_premission "Chroot in the new system environment? "; then
@@ -299,12 +332,12 @@ if execution_premission "Chroot in the new system environment? "; then
 	profile="\
 	etc-update; env-update && source /etc/profile \n\
 	locale-gen; env-update && source /etc/profile \n\
-	PS1=chroot\[\033[01;36m\]> \n\
 	echo -e \"\nNow you are in chrooted environment.\
 	\nselect default languge via eselect locale set\
 	\nsync portage tree and merge packages you need\" \n\
 	rm -rf ~/.bash_login"
 	echo -e ${profile} > ${work_dir}/root/.bash_login
+	chmod +x ${work_dir}/root/.bash_login
 	env -i HOME=/root TERM=$TERM SHELL=/bin/bash chroot ${work_dir} /root/.bash_login
 	env -i HOME=/root TERM=$TERM SHELL=/bin/bash chroot ${work_dir} /bin/bash --login
 fi
